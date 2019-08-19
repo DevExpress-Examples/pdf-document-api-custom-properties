bbfddb4b5133138970ab9927c9285f07f2ccd1fb
Poline Fedorova <polina.fedorova@devexpress.com>
2019-08-19 12:32:38 +0300

    add files

---
+++ CS/pdf-custom-properties/Program.cs
@@ -0,0 +1,37 @@
+﻿using DevExpress.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdf_custom_properties
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            using (PdfDocumentProcessor pdfProcessor = new PdfDocumentProcessor())
+            {
+                pdfProcessor.LoadDocument("PageDeletion.pdf");
+                ModifyCustomProperties(pdfProcessor.Document);
+                pdfProcessor.SaveDocument("Result.pdf");
+                Process.Start("Result.pdf");
+            }
+        }
+
+        private static void ModifyCustomProperties(PdfDocument document)
+        {
+            //Add new property
+            document.CustomProperties.Add("NumberOfCopies", "3");
+
+            //Modify the CompanyEmail property value:
+            if (document.CustomProperties.ContainsKey("CompanyEmail"))
+                document.CustomProperties["CompanyEmail"] = "clientservices@devexpress.com";
+
+            //Remove the HasImages property:
+            document.CustomProperties.Remove("HasImages");
+        }
+    }
+}
---
+++ VB/pdf-custom-properties/Program.vb
@@ -0,0 +1,33 @@
+﻿Imports DevExpress.Pdf
+Imports System
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace pdf_custom_properties
+	Friend Class Program
+		Shared Sub Main(ByVal args() As String)
+			Using pdfProcessor As New PdfDocumentProcessor()
+				pdfProcessor.LoadDocument("PageDeletion.pdf")
+				ModifyCustomProperties(pdfProcessor.Document)
+				pdfProcessor.SaveDocument("Result.pdf")
+				Process.Start("Result.pdf")
+			End Using
+		End Sub
+
+		Private Shared Sub ModifyCustomProperties(ByVal document As PdfDocument)
+			'Add new property
+			document.CustomProperties.Add("NumberOfCopies", "3")
+
+			'Modify the CompanyEmail property value:
+			If document.CustomProperties.ContainsKey("CompanyEmail") Then
+				document.CustomProperties("CompanyEmail") = "clientservices@devexpress.com"
+			End If
+
+			'Remove the HasImages property:
+			document.CustomProperties.Remove("HasImages")
+		End Sub
+	End Class
+End Namespace
